a90dae7e4172f96b1ac4de4b0422d15d33654fe3
anglobaltech <mqtltech@gmail.com>
2026-01-20 16:47:40 +0530

    sitemap update

--- app/sitemap.xml/route.js
+++ app/sitemap.xml/route.js
@@ -1,28 +1,52 @@
 import { NextResponse } from 'next/server'
+import fs from 'fs'
+import path from 'path'
 
 export async function GET() {
   const baseUrl = 'https://anglobalservices.com'
+  const appDir = path.join(process.cwd(), 'app')
 
-  const pages = [
-    '/',
-    '/about',
-    '/contact',
-      ]
+  function getRoutes(dir, basePath = '') {
+    const entries = fs.readdirSync(dir, { withFileTypes: true })
 
-  const urls = pages
-    .map(
-      (path) => `
+    let routes = []
+
+    for (const entry of entries) {
+      if (entry.name.startsWith('_')) continue
+      if (entry.name === 'api') continue
+      if (entry.name === 'sitemap.xml') continue
+
+      const fullPath = path.join(dir, entry.name)
+      const routePath = `${basePath}/${entry.name}`
+
+      if (entry.isDirectory()) {
+        const pageFile = path.join(fullPath, 'page.js')
+        if (fs.existsSync(pageFile)) {
+          routes.push(routePath)
+        }
+        routes.push(...getRoutes(fullPath, routePath))
+      }
+    }
+    return routes
+  }
+
+  const pages = getRoutes(appDir)
+
+  const urls = pages.map(
+    (route) => `
     <url>
-      <loc>${baseUrl}${path}</loc>
+      <loc>${baseUrl}${route === '/page' ? '' : route}</loc>
       <changefreq>weekly</changefreq>
       <priority>0.8</priority>
-    </url>
-  `
-    )
-    .join('')
+    </url>`
+  ).join('')
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>${baseUrl}/</loc>
+      <priority>1.0</priority>
+    </url>
     ${urls}
   </urlset>`
 
